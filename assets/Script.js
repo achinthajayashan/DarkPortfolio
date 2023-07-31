e3e797a205a7563610f3c320251c851e2bdd5029
achinthajayashan <achintha.uns.2018@gmail.com>
2023-07-31 23:35:52 +0530

    dark

--- assets/Script.js
+++ assets/Script.js
@@ -1,8 +1,25 @@
+$('#btnHome').css("background-color","white");
+$('#btnHome').css("color","black");
+
+$('#aboutSec').hide();
+
 $('#btnHome').click(function () {
     $('#homeSec').show();
+    $('#aboutSec').hide();
+    $(this).css("background-color","white");
+    $(this).css("color","black");
+
+    $('#btnAbout').css("background-color","black");
+    $('#btnAbout').css("color","white");
 });
 
 $('#btnAbout').click(function () {
     $('#homeSec').hide();
+    $('#aboutSec').show();
+    $(this).css("background-color","white");
+    $(this).css("color","black");
+
+    $('#btnHome').css("background-color","black");
+    $('#btnHome').css("color","white");
 });
 
