17fb47977d36083edca256ab4628e5a6e1d037ac
achinthajayashan <achintha.uns.2018@gmail.com>
2023-08-02 00:06:22 +0530

    services finished

--- assets/Script.js
+++ assets/Script.js
@@ -2,23 +2,44 @@ $('#btnHome').css("background-color","white");
 $('#btnHome').css("color","black");
 
 $('#aboutSec').hide();
+$('#serviceSec').hide();
 
 $('#btnHome').click(function () {
     $('#homeSec').show();
     $('#aboutSec').hide();
+    $('#serviceSec').hide();
     $(this).css("background-color","white");
     $(this).css("color","black");
 
     $('#btnAbout').css("background-color","black");
     $('#btnAbout').css("color","white");
+    $('#btnService').css("background-color","black");
+    $('#btnService').css("color","white");
 });
 
 $('#btnAbout').click(function () {
     $('#homeSec').hide();
     $('#aboutSec').show();
+    $('#serviceSec').hide();
     $(this).css("background-color","white");
     $(this).css("color","black");
 
+    $('#btnHome').css("background-color","black");
+    $('#btnHome').css("color","white");
+    $('#btnService').css("background-color","black");
+    $('#btnService').css("color","white");
+});
+
+$('#btnService').click(function () {
+    $('#homeSec').hide();
+    $('#aboutSec').hide();
+    $('#serviceSec').show();
+
+    $(this).css("background-color","white");
+    $(this).css("color","black");
+
+    $('#btnAbout').css("background-color","black");
+    $('#btnAbout').css("color","white");
     $('#btnHome').css("background-color","black");
     $('#btnHome').css("color","white");
 });
