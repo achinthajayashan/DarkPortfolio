340aa4fc448d42981d02dcfe7327876e30c76ff3
achinthajayashan <achintha.uns.2018@gmail.com>
2023-08-04 11:15:55 +0530

    nav menu

--- assets/Script.js
+++ assets/Script.js
@@ -1,106 +1,121 @@
-$('#btnHome').css("background-color","white");
-$('#btnHome').css("color","black");
+$('.btnHome').css("background-color","white");
+$('.btnHome').css("color","black");
+
+$('#sideBar').hide();
 
 $('#aboutSec').hide();
 $('#serviceSec').hide();
 $('#portfolioSec').hide();
 $('#contactSec').hide();
 
-$('#btnHome').click(function () {
+$('.btnHome').click(function () {
     $('#homeSec').show();
     $('#aboutSec').hide();
     $('#serviceSec').hide();
     $('#portfolioSec').hide();
     $('#contactSec').hide();
+    $('#sideBar').hide();
     $(this).css("background-color","white");
     $(this).css("color","black");
 
-    $('#btnAbout').css("background-color","black");
-    $('#btnAbout').css("color","white");
-    $('#btnService').css("background-color","black");
-    $('#btnService').css("color","white");
-    $('#btnPortfolio').css("background-color","black");
-    $('#btnPortfolio').css("color","white");
-    $('#btnContact').css("background-color","black");
-    $('#btnContact').css("color","white");
+    $('.btnAbout').css("background-color","black");
+    $('.btnAbout').css("color","white");
+    $('.btnService').css("background-color","black");
+    $('.btnService').css("color","white");
+    $('.btnPortfolio').css("background-color","black");
+    $('.btnPortfolio').css("color","white");
+    $('.btnContact').css("background-color","black");
+    $('.btnContact').css("color","white");
 });
 
-$('#btnAbout').click(function () {
+$('.btnAbout').click(function () {
     $('#homeSec').hide();
     $('#aboutSec').show();
     $('#serviceSec').hide();
     $('#portfolioSec').hide();
     $('#contactSec').hide();
+    $('#sideBar').hide();
     $(this).css("background-color","white");
     $(this).css("color","black");
 
-    $('#btnHome').css("background-color","black");
-    $('#btnHome').css("color","white");
-    $('#btnService').css("background-color","black");
-    $('#btnService').css("color","white");
-    $('#btnPortfolio').css("background-color","black");
-    $('#btnPortfolio').css("color","white");
-    $('#btnContact').css("background-color","black");
-    $('#btnContact').css("color","white");
+    $('.btnHome').css("background-color","black");
+    $('.btnHome').css("color","white");
+    $('.btnService').css("background-color","black");
+    $('.btnService').css("color","white");
+    $('.btnPortfolio').css("background-color","black");
+    $('.btnPortfolio').css("color","white");
+    $('.btnContact').css("background-color","black");
+    $('.btnContact').css("color","white");
 });
 
-$('#btnService').click(function () {
+$('.btnService').click(function () {
     $('#homeSec').hide();
     $('#aboutSec').hide();
     $('#serviceSec').show();
     $('#portfolioSec').hide();
     $('#contactSec').hide();
+    $('#sideBar').hide();
 
     $(this).css("background-color","white");
     $(this).css("color","black");
 
-    $('#btnAbout').css("background-color","black");
-    $('#btnAbout').css("color","white");
-    $('#btnHome').css("background-color","black");
-    $('#btnHome').css("color","white");
-    $('#btnPortfolio').css("background-color","black");
-    $('#btnPortfolio').css("color","white");
-    $('#btnContact').css("background-color","black");
-    $('#btnContact').css("color","white");
+    $('.btnAbout').css("background-color","black");
+    $('.btnAbout').css("color","white");
+    $('.btnHome').css("background-color","black");
+    $('.btnHome').css("color","white");
+    $('.btnPortfolio').css("background-color","black");
+    $('.btnPortfolio').css("color","white");
+    $('.btnContact').css("background-color","black");
+    $('.btnContact').css("color","white");
 });
 
-$('#btnPortfolio').click(function () {
+$('.btnPortfolio').click(function () {
     $('#homeSec').hide();
     $('#aboutSec').hide();
     $('#serviceSec').hide();
     $('#portfolioSec').show();
     $('#contactSec').hide();
+    $('#sideBar').hide();
 
     $(this).css("background-color","white");
     $(this).css("color","black");
 
-    $('#btnAbout').css("background-color","black");
-    $('#btnAbout').css("color","white");
-    $('#btnHome').css("background-color","black");
-    $('#btnHome').css("color","white");
-    $('#btnService').css("background-color","black");
-    $('#btnService').css("color","white");
-    $('#btnContact').css("background-color","black");
-    $('#btnContact').css("color","white");
+    $('.btnAbout').css("background-color","black");
+    $('.btnAbout').css("color","white");
+    $('.btnHome').css("background-color","black");
+    $('.btnHome').css("color","white");
+    $('.btnService').css("background-color","black");
+    $('.btnService').css("color","white");
+    $('.btnContact').css("background-color","black");
+    $('.btnContact').css("color","white");
 });
 
-$('#btnContact').click(function () {
+$('.btnContact').click(function () {
     $('#homeSec').hide();
     $('#aboutSec').hide();
     $('#serviceSec').hide();
     $('#portfolioSec').hide();
     $('#contactSec').show();
+    $('#sideBar').hide();
 
     $(this).css("background-color","white");
     $(this).css("color","black");
 
-    $('#btnAbout').css("background-color","black");
-    $('#btnAbout').css("color","white");
-    $('#btnHome').css("background-color","black");
-    $('#btnHome').css("color","white");
-    $('#btnPortfolio').css("background-color","black");
-    $('#btnPortfolio').css("color","white");
-    $('#btnService').css("background-color","black");
-    $('#btnService').css("color","white");
+    $('.btnAbout').css("background-color","black");
+    $('.btnAbout').css("color","white");
+    $('.btnHome').css("background-color","black");
+    $('.btnHome').css("color","white");
+    $('.btnPortfolio').css("background-color","black");
+    $('.btnPortfolio').css("color","white");
+    $('.btnService').css("background-color","black");
+    $('.btnService').css("color","white");
 });
+
+$('#barMenu').click(function () {
+    $('#sideBar').show();
+});
+
+$('#close').click(function () {
+    $('#sideBar').hide();
+})
 
